4cbf8638a113a5a27811838578f15e8c1e1ab2a3
WhatTheSlime <selim.lanouar@gmail.com>
2019-12-18 15:37:07 +0100

    Fix setup.py

--- setup.py
+++ setup.py
@@ -1,22 +1,14 @@
-import setuptools
+from setuptools import setup
 
-with open("README.md", "r") as f:
-    long_description = f.read()
-
-setuptools.setup(
-    name="aiorequests", # Replace with your own username
-    version="0.0.1",
-    author="Slime",
-    author_email="selim.lanouar@gmail.com",
-    description="Requests lib wrapper to make asynchronus requests and provide useful requests",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/WhatTheSlime/aiorequests",
-    packages=setuptools.find_packages(),
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
-    python_requires='>=3.6',
+setup(
+    name='aiorequests-whattheslime',
+    version='0.0.1',
+    description='Request wrapper',
+    url='git@github.com:WhatTheSlime/aiorequests.git',
+    author='Sélim Lanouar',
+    author_email='selim.lanouar@gmail.com',
+    license='unlicense',
+    packages=['aiorequests'],
+    zip_safe=False
 )
+
